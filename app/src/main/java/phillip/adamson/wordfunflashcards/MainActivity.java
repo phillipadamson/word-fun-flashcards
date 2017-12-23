81eaa3d507570968e437691c5340e743852c96d1
Phillip Adamson <padamson@avantlink.com>
2017-12-22 21:23:33 -0700

    This is the initial commit.

---
+++ app/src/main/java/phillip/adamson/wordfunflashcards/MainActivity.java
@@ -0,0 +1,22 @@
+package phillip.adamson.wordfunflashcards;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+    }
+
+}
